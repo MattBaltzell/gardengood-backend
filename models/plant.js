e1490af0b0c2b950dbadfe247a40a748f3d97bb3
Matt Baltzell <matthew.j.baltzell@gmail.com>
2023-02-24 15:36:54 -0600

    add methods to Plant model: - findAll - get (by id)

--- models/plant.js
+++ models/plant.js
@@ -3,6 +3,45 @@
 const db = require("../db");
 const { NotFoundError } = require("../expressError");
 
-class Plant {}
+class Plant {
+  /** Find all plants */
+  static async findAll() {
+    const plants = await db.query(
+      `
+    SELECT id,
+        name, 
+        species, 
+        img_url AS "imgUrl",
+        is_perrenial AS "isPerrenial",
+        description,
+        days_to_maturity_min AS "daysToMaturityMin",
+        days_to_maturity_max AS "daysToMaturityMax"
+    FROM plants`
+    );
+    return plants.rows;
+  }
 
-module.exports = Plants;
+  /** Given a plant id, return data about a plant */
+
+  static async get(id) {
+    const plant = await db.query(
+      `
+    SELECT id,
+        name, 
+        species, 
+        img_url AS "imgUrl",
+        is_perrenial AS "isPerrenial",
+        description,
+        days_to_maturity_min AS "daysToMaturityMin",
+        days_to_maturity_max AS "daysToMaturityMax"
+    FROM plants
+    WHERE id = $1`,
+      [id]
+    );
+
+    if (!plant.rows[0]) throw new NotFoundError(`No plant: ${id}`);
+    return plant.rows[0];
+  }
+}
+
+module.exports = Plant;
