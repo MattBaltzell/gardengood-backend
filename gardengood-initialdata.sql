51039f683b3d6cb194e75796083c045548e3f81d
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-02-25 16:27:32 -0600

    create initial data in db for: - seasons - sunlight - instruction types

---
+++ gardengood-initialdata.sql
@@ -0,0 +1,26 @@
+INSERT INTO seasons (name)
+    VALUES
+        ('early spring'),('spring'),
+        ('late spring'),
+        ('early summer'),
+        ('summer'),
+        ('late summer'),
+        ('early fall') ,
+        ('fall'),
+        ('late fall'),
+        ('early winter'),
+        ('winter'),
+        ('late winter');
+
+INSERT INTO sunlight (name,description)
+    VALUES
+        ('Full Sun', 'Full sun is six or more hours of direct sunlight per day.'),
+        ('Partial Sun', 'Partial sun is between four and six hours of sun a day.'),
+        ('Partial Shade', 'Partial shade is two to four hours of sun per day.'),
+        ('Shade', 'Shade, in gardening terms, means less than two hours of sunlight a day.');
+
+INSERT INTO instruction_types (name)
+    VALUES
+        ('Planting'),
+        ('Pruning'),
+        ('Watering');
