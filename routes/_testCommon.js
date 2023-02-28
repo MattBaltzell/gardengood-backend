f20043d0c0a8492eba5edf87d2e5b13a1942d7a7
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-02-28 00:50:53 -0600

    test users and auth routes

--- routes/_testCommon.js
+++ routes/_testCommon.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const db = require("../db.js");
+const User = require("../models/user");
 const Plant = require("../models/plant");
+const { createToken } = require("../helpers/tokens");
 
 async function commonBeforeAll() {
   // noinspection SqlWithoutWhere
@@ -12,6 +14,38 @@ async function commonBeforeAll() {
   await db.query("DELETE FROM plants_seasons");
   // noinspection SqlWithoutWhere
   await db.query("DELETE FROM plants");
+  // noinspection SqlWithoutWhere
+  await db.query("DELETE FROM users");
+
+  await User.register({
+    username: "u1",
+    firstName: "U1F",
+    lastName: "U1L",
+    email: "user1@user.com",
+    password: "password1",
+    zipCode: "36830",
+    isAdmin: false,
+  });
+
+  await User.register({
+    username: "u2",
+    firstName: "U2F",
+    lastName: "U2L",
+    email: "user2@user.com",
+    password: "password2",
+    zipCode: "36830",
+    isAdmin: false,
+  });
+
+  await User.register({
+    username: "u3",
+    firstName: "U3F",
+    lastName: "U3L",
+    email: "user3@user.com",
+    password: "password3",
+    zipCode: "36830",
+    isAdmin: false,
+  });
 
   await Plant.create({
     name: "Routes Test Plant 1",
@@ -59,9 +93,16 @@ async function commonAfterAll() {
   await db.end();
 }
 
+const u1Token = createToken({ username: "u1", isAdmin: false });
+const u2Token = createToken({ username: "u2", isAdmin: false });
+const adminToken = createToken({ username: "admin", isAdmin: true });
+
 module.exports = {
   commonBeforeAll,
   commonBeforeEach,
   commonAfterEach,
   commonAfterAll,
+  u1Token,
+  u2Token,
+  adminToken,
 };
