22bc62405dce118ff84e0cab99554bcf1b72d1d9
Matt Baltzell <matthew.j.baltzell@gmail.com>
2023-03-03 16:00:56 -0600

    update bad request message for user

--- models/user.js
+++ models/user.js
@@ -75,7 +75,7 @@ class User {
     );
 
     if (duplicateCheck.rows[0]) {
-      throw new BadRequestError(`Duplicate username: ${username}`);
+      throw new BadRequestError(`${username} is already taken.`);
     }
 
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
