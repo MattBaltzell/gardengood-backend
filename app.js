8aba76d4f6d2c780cf1e397854b6d841e6bf86d3
Matt Baltzell <matthew.j.baltzell@gmail.com>
2023-03-07 15:54:12 -0600

    add plant, crop and bed routes to app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const plantRoutes = require("./routes/plants");
+const cropRoutes = require("./routes/crops");
+const bedRoutes = require("./routes/beds");
+const gardenRoutes = require("./routes/gardens");
 const userRoutes = require("./routes/users");
 
 const app = express();
@@ -19,6 +22,9 @@ app.use(authenticateJWT);
 // app.use(/**route name */)
 app.use("/auth", authRoutes);
 app.use("/plants", plantRoutes);
+app.use("/crops", cropRoutes);
+app.use("/beds", bedRoutes);
+app.use("/gardens", gardenRoutes);
 app.use("/users", userRoutes);
 
 /** Handle 404 errors -- this matches everything */
