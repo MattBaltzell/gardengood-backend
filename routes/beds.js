b31975a49ad08e2b8342ef5c31fe224cb74e47d8
Matthew Baltzell <matthew.j.baltzell@gmail.com>
2023-03-07 19:05:45 -0600

    create GET,POST, and DELETE routes for beds

--- routes/beds.js
+++ routes/beds.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const { ensureLoggedIn, ensureAdmin } = require("../middleware/auth");
 const jsonschema = require("jsonschema");
-// const bedNewSchema = require("../schemas/bedNew.json");
+const bedNewSchema = require("../schemas/bedNew.json");
 const Bed = require("../models/bed");
 const { BadRequestError } = require("../expressError");
 const router = express.Router();
@@ -17,34 +17,71 @@ const router = express.Router();
  * Authorization required: logged in
  **/
 
-// router.post("/", ensureLoggedIn, async function (req, res, next) {
-//   try {
-//     const validator = jsonschema.validate(req.body, bedNewSchema);
-//     if (!validator.valid) {
-//       const errs = validator.errors.map((e) => e.stack);
-//       throw new BadRequestError(errs);
-//     }
-//     const bed = await Bed.create(req.body);
-//     return res.status(201).json(bed);
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+router.post("/:gardenId", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const validator = jsonschema.validate(req.body, bedNewSchema);
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+    const { name } = req.body;
+    const { gardenId } = req.params;
+    const bed = await Bed.create({ name, gardenId });
+    return res.status(201).json(bed);
+  } catch (err) {
+    return next(err);
+  }
+});
 
-/** GET /[username] => { user }
+/** GET /[id] => { bed }
  *
  * Returns { id, plantId, bedId, qty, plantedAt }
  *
  * Authorization required: logged in
  **/
 
-// router.get("/:id", ensureLoggedIn, async function (req, res, next) {
-//   try {
-//     const bed = await Bed.get(req.params.id);
-//     return res.json({ bed });
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+router.get("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const bed = await Bed.get(req.params.id);
+    return res.json({ bed });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** GET /garden/[gardenId] => [{ bed },{ bed },...]
+ *
+ * Returns [{ id, name, gardenId, }, ...]
+ *
+ * Authorization required: logged in
+ **/
+
+router.get(
+  "/garden/:gardenId",
+  ensureLoggedIn,
+  async function (req, res, next) {
+    try {
+      const beds = await Bed.findAll(req.params.gardenId);
+      return res.json({ beds });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** DELETE /[id] => { bed }
+ *
+ * Returns { deleted: name }
+ *
+ * Authorization required: logged in
+ **/
+router.delete("/:id", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const bed = await Bed.remove(req.params.id);
+    return res.json({ deleted: bed.name });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
