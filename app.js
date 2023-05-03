e13c085bf774558d3a04f22fd5e283a11b8b1e63
Matt Baltzell <matthew.j.baltzell@gmail.com>
2023-05-03 13:12:52 -0500

    remove crops, beds, and garden routes - These routes were moved into user routes

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const plantRoutes = require("./routes/plants");
-const cropRoutes = require("./routes/crops");
-const bedRoutes = require("./routes/beds");
-const gardenRoutes = require("./routes/gardens");
+
 const userRoutes = require("./routes/users");
 
 const app = express();
@@ -22,9 +20,6 @@ app.use(authenticateJWT);
 // app.use(/**route name */)
 app.use("/auth", authRoutes);
 app.use("/plants", plantRoutes);
-app.use("/crops", cropRoutes);
-app.use("/beds", bedRoutes);
-app.use("/gardens", gardenRoutes);
 app.use("/users", userRoutes);
 
 /** Handle 404 errors -- this matches everything */
