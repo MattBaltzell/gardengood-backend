b58f69729fa9387497bbd0dbf7e116a3c647d0a0
Matt Baltzell <matthew.j.baltzell@gmail.com>
2023-05-05 07:04:40 -0500

    create Garden.update method

--- models/garden.js
+++ models/garden.js
@@ -94,10 +94,39 @@ class Garden {
     return gardens;
   }
 
-  // addUser
-  // removeUser
+  /** Update garden data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain
+   * all the fields; this only changes provided ones.
+   *
+   * Data can include:
+   *   { name, description }
+   *
+   * Returns { id, name, description }
+   *
+   * Throws NotFoundError if not found.
+   *
+   */
 
-  // delete
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(data, {});
+    const idVarIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE gardens 
+                      SET ${setCols} 
+                      WHERE id = ${idVarIdx} 
+                      RETURNING id,
+                                name,
+                                description`;
+    const result = await db.query(querySql, [...values, id]);
+    const garden = result.rows[0];
+
+    if (!garden) throw new NotFoundError(`No garden: ${id}`);
+
+    return garden;
+  }
+
+  /** Delete given garden from database; returns undefined. */
 
   static async remove(id) {
     const gardenRes = await db.query(
